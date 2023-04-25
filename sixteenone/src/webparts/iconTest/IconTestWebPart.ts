de7d2b308598bec73e05132af58aa6ca28fc5267
Chris Kent <8364109+thechriskent@users.noreply.github.com>
2023-04-25 12:24:47 -0400

    Initial commit

---
+++ sixteenone/src/webparts/iconTest/IconTestWebPart.ts
@@ -0,0 +1,72 @@
+import { Version } from '@microsoft/sp-core-library';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import { IReadonlyTheme } from '@microsoft/sp-component-base';
+
+import styles from './IconTestWebPart.module.scss';
+import * as strings from 'IconTestWebPartStrings';
+
+import { initializeIcons } from '@uifabric/icons';
+import { getIconClassName } from '@uifabric/styling';
+
+initializeIcons();
+
+export interface IIconTestWebPartProps {
+  description: string;
+}
+
+export default class IconTestWebPart extends BaseClientSideWebPart<IIconTestWebPartProps> {
+
+  public render(): void {
+    this.domElement.innerHTML = `
+    <div class=${styles.iconTest}>
+      <i class="${getIconClassName('FangBody')}"></i>
+    </div>`;
+  }
+
+  protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
+    if (!currentTheme) {
+      return;
+    }
+
+    const {
+      semanticColors
+    } = currentTheme;
+
+    if (semanticColors) {
+      this.domElement.style.setProperty('--bodyText', semanticColors.bodyText || null);
+      this.domElement.style.setProperty('--link', semanticColors.link || null);
+      this.domElement.style.setProperty('--linkHovered', semanticColors.linkHovered || null);
+    }
+
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
